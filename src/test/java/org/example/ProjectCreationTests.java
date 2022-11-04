8962a97874a32e851ec774ef66f9454e601f86fb
KBuczek1 <kbuczek1@t-mobile.pl>
2022-11-04 11:09:18 +0100

    added second test, and refactor of existing code

--- src/test/java/org/example/ProjectCreationTests.java
+++ src/test/java/org/example/ProjectCreationTests.java
@@ -1,21 +1,152 @@
 package org.example;
 
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import static java.lang.String.format;
 
 public class ProjectCreationTests {
 
+    String projectName = "Lepsze szkolenie RestAssured";
+
+
+    @BeforeAll
+    public static void setup() {
+
+        RequestSpecBuilder builder = new RequestSpecBuilder();
+        builder.setBaseUri("https://api.todoist.com")
+               .setBasePath("/rest/v2")
+               .addHeader("Authorization", "Bearer 3d805d60a854c15ef18a760c629963b5030fd4a2")
+//               .log(LogDetail.ALL)
+                .build();
+        RestAssured.requestSpecification = builder.build();
+
+//        RestAssured.responseSpecification = new ResponseSpecBuilder()
+//        .log(LogDetail.ALL).build();
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails(); // logi jeśli asercje sfailują
+    }
+
     @Test
     public void userCanCreateANewProject() {
+//        var projectName = "Lepsze szkolenie RestAssured";
+
+        var projectId = userCreatesANewProject(projectName);
+        userChecksProjectDetails(projectId,projectName);
+        userChecksAllProjectsList(projectId, projectName);
+
+    }
+
+    @Test
+    public void userCanAddTaskToTheProject() {
+        var projectName = "Projekt do tworzenia zadań";
+        var taskName = "Pierwsze zadanie w projekcie";
+        var projectId = userCreatesANewProject(projectName);
+        String taskId = userCreatesANewTask(taskName, projectId);
+        userChecksIfTaskIsCreated(taskId,taskName);
+        userChecksAllTasksList(taskId,taskName);
+
+    }
+
+    private void userChecksAllTasksList(String taskId, String taskName) {
+
         RestAssured
                 .given()
-                .baseUri("https://api.todoist.com")
-                .basePath("/rest/v2")
-                .header("Authorization", "Bearer 3d805d60a854c15ef18a760c629963b5030fd4a2")
+                .log().all()
+                .when()
+                .get("/tasks")
+                .then()
+                .log().all()
+                .assertThat()
+                .statusCode(200)
+                .body(
+                        format("find{it.id == \"%s\"}.content", taskId),
+                        Matchers.equalTo(taskName));
+    }
+
+    private void userChecksIfTaskIsCreated(String taskId, String taskName) {
+        RestAssured
+                .given()
+                .pathParam("id", taskId)
+                .log().all()
+                .when()
+                .get("/tasks/{id}")
+                .then()
+                .log().all()
+                .assertThat()
+                .statusCode(200)
+                .body("id", Matchers.equalTo(taskId))
+                .body("content", Matchers.equalTo(taskName));
+    }
+
+
+    private String userCreatesANewTask(String taskName, String projectId) {
+
+
+
+        String taskId = RestAssured
+                .given()
                 .contentType(ContentType.JSON)
-                .body("{\"name\":\"Szkolenie Rest API\"}")
+                .body(format("{\"content\": \"%s\", \"project_id\": \"%s\"}",taskName,projectId))
+                .log().all()
+                .when()
+                .post("/tasks")
+                .then()
+                .log().all()
+                .assertThat()
+                .statusCode(200)
+                .body("content", Matchers.equalTo(taskName))
+                .header("Content-Type", Matchers.equalTo("application/json"))
+                .and()
+                .extract().path("id");
+        return taskId;
+
+    }
+
+    private void userChecksAllProjectsList(String projectId, String projectName) {
+
+        RestAssured
+                .given()
+                .log().all()
+                .when()
+                .get("/projects")
+                .then()
+                .log().all()
+                .assertThat()
+                .statusCode(200)
+                .body(
+                        format("find{it.id == \"%s\"}.name", projectId),
+                        Matchers.equalTo(projectName));
+
+    }
+
+    private void userChecksProjectDetails(String projectId, String projectName) {
+
+        RestAssured
+                .given()
+                .pathParam("id", projectId)
+                .log().all()
+                .when()
+                .get("/projects/{id}")
+                .then()
+                .log().all()
+                .assertThat()
+                .statusCode(200)
+                .body("id", Matchers.equalTo(projectId))
+                .body("name", Matchers.equalTo(projectName));
+    }
+
+    private String userCreatesANewProject(String projectName) {
+
+        String projectId = RestAssured
+                .given()
+                .contentType(ContentType.JSON)
+                .body("{\"name\":\""+ projectName + "\"}")
                 .log().all()
                 .when()
                 .post("/projects")
@@ -23,22 +154,10 @@ public class ProjectCreationTests {
                 .log().all()
                 .assertThat()
                 .statusCode(200)
-                .body("name", Matchers.equalTo("Szkolenie Rest API"))
-                .header("Content-Type", Matchers.equalTo("application/json"));
-
-        RestAssured
-                .given()
-                .baseUri("https://api.todoist.com")
-                .basePath("/rest/v2")
-                .header("Authorization", "Bearer 3d805d60a854c15ef18a760c629963b5030fd4a2")
-                .log().all()
-                .when()
-                .get("/projects/2301674932")
-                .then()
-                .log().all()
-                .assertThat()
-                .statusCode(200)
-                .body("id", Matchers.equalTo("2301674932"))
-                .body("name", Matchers.equalTo("Szkolenie Rest API"));
+                .body("name", Matchers.equalTo(projectName))
+                .header("Content-Type", Matchers.equalTo("application/json"))
+                .and()
+                .extract().path("id");
+             return projectId;
     }
 }
